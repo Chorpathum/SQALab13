2c86665cd199ca35beee8f323bbd722fbde3f03a
pongsatonNei <pongsaton.n@kkumail.com>
2022-10-11 20:40:35 +0700

    input int 2

--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -2,16 +2,32 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import main.FizzBuzz;
 
 class FizzBuzzTest {
-
+	FizzBuzz FiBu;
+	
+	@BeforeEach
+	void setUp() {
+		FiBu = new FizzBuzz() ;
+	}
 	@Test
 	void FizzBuzzTest1() {
-		FizzBuzz FiBu = new FizzBuzz() ;
+		
 		assertEquals(1,FiBu.FizzBuzzcal(1));
 	}
+	/**/
+	@DisplayName("input int 2")
+	@Test
+	void FizzBuzzTest2() {
+		
+		assertEquals('2',FiBu.FizzBuzzcal(2));
+		
+	}
+	
 
 }
