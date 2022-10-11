667ff9596f9efd486630ade7b387ab06579712d1
Chorpathum <Chorpathum.u@kkumail.com>
2022-10-11 22:12:47 +0700

    mod 3 and mod 5

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -2,12 +2,17 @@ package main;
 
 public class FizzBuzz {
 	 public String FizzBuzzcal(Integer input) {
-		 //หาร3ลงตัว return Fizz
-		 if (input%3==0) {
-			 return "Fizz";
+		 //หาร3ลงตัว หาร5ลงตัว return FizzBuzz
+		 if (input%3==0 && input%5==0){
+			 return "FizzBuzz";
 		 }
+		 //หาร5ลงตัว return Buzz
 		 else if (input%5==0){
 			 return "Buzz";
+		 }
+		 //หาร3ลงตัว return Fizz
+		 else if (input%3==0) {
+			 return "Fizz";
 		 }
 		 else {
 			 return input.toString();
