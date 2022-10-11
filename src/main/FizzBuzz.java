86f22e368f27d3fafd0ea28ece0cbe46dc42b7e9
Chorpathum <Chorpathum.u@kkumail.com>
2022-10-11 17:55:54 +0700

    FizzBuzz1 function

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -1,5 +1,7 @@
 package main;
 
 public class FizzBuzz {
-
+	 public double FizzBuzzcal(double input) {
+		 return input;
+	 }
 }
--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.FizzBuzz;
+
 class FizzBuzzTest {
 
 	@Test
-	void test() {
+	void FizzBuzzTest1() {
 		FizzBuzz FiBu = new FizzBuzz() ;
 		assertEquals(1,FiBu.FizzBuzzcal(1));
 	}
